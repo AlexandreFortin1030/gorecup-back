47242a1ecdd24e892d693fbb3d1aac35c0812aad
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-06-25 12:20:28 +0200

    erokekrekr

--- server/venv/bin/activate.fish
+++ server/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/antoine/Work/Websites/gorecup-back/server app/venv"
+set -gx VIRTUAL_ENV "/home/antoine/Work/Websites/gorecup-back/server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
